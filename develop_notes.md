8a24b8baa6339ee180af0e2cbdc49af01749a9d9
zhenxing.Yan <zhenxing.yan@vedaleon.com>
2018-11-09 17:54:14 +1100

    exercise

---
+++ develop_notes.md
@@ -0,0 +1,12 @@
+# 1. webpack
+
+1.1 installation
+
+npm install --save-dev webpack
+
+loading css : style-loader css-loader
+
+loading images : file-loader
+
+loading fonts : file-loader
+
--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-/* eslint-disable no-console */
-console.log('hi');
+import _ from 'lodash';
+import './style.css';
+import Icon from './img/icon.jpg';
+
+function component() {
+    let element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    return element;
+  }
+  
+  document.body.appendChild(component());
